050f408344e7dba590b2a0dfae19c23ac164507d
Markus Spanring <markusspanring@gmail.com>
2016-11-17 18:00:01 +0100

    reset W and QCD histos

--- src/CreateHistos.cc
+++ src/CreateHistos.cc
@@ -98,8 +98,8 @@ void CreateHistos::run(){
     //Int_t nentries = min( Int_t(NtupleView->fChain->GetEntries()), Int_t( 1000000 ) );
     cout<<"The input chain contains: "<<nentries<<" events."<<endl;
     float perc;
-    //for (Int_t jentry=0; jentry<100000;jentry++){
-    for (Int_t jentry=0; jentry<nentries;jentry++){       
+    for (Int_t jentry=0; jentry<100000;jentry++){
+    //for (Int_t jentry=0; jentry<nentries;jentry++){       
 
       if(jentry % 200000 == 0){
         if(nentries > 0){
@@ -483,6 +483,13 @@ void CreateHistos::CreateQCD_osw(TString strVar, TString cat){
   this->GetHistbyName("QCD_OSW"+sub,strVar)->Add( this->GetHistbyName("SS_W_TT"+sub,strVar), -1 );
   this->GetHistbyName("QCD_OSW"+sub,strVar)->Add( this->GetHistbyName("SS_W_W"+sub,strVar),  -1 );
   this->GetHistbyName("QCD_OSW"+sub,strVar)->Scale( this->QCD_OSSS(cat) );
+
+  if(reset){
+    int entries = this->GetHistbyName("QCD_OSW"+sub,strVar)->GetNbinsX();
+    for(int i = 1; i < entries; i++){
+      if( this->GetHistbyName("QCD_OSW"+sub,strVar)->GetBinContent(i) < 0 ) this->GetHistbyName("QCD_OSW"+sub,strVar)->SetBinContent(i,0.);
+    }
+  }
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -500,6 +507,14 @@ void CreateHistos::CreateW(TString strVar, TString cat){
 
   this->GetHistbyName("W"+sub,strVar)->Add( this->GetHistbyName("W_OSW"+sub,strVar) );
   this->GetHistbyName("W"+sub,strVar)->Multiply( this->GetHistbyName("HL"+sub, strVar) );
+
+  if(reset){
+    int entries = this->GetHistbyName("W"+sub,strVar)->GetNbinsX();
+    for(int i = 1; i < entries; i++){
+      if( this->GetHistbyName("W"+sub,strVar)->GetBinContent(i) < 0 ) this->GetHistbyName("W"+sub,strVar)->SetBinContent(i,0.);
+    }
+  }
+
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -525,6 +540,13 @@ void CreateHistos::CreateQCD(TString strVar, TString cat){
   this->GetHistbyName("QCD"+sub,strVar)->Add( this->GetHistbyName("SS_Low_relaxed_TT"+sub,strVar), -1 );
   this->GetHistbyName("QCD"+sub,strVar)->Add( this->GetHistbyName("SS_Low_relaxed_W"+sub,strVar),  -1 );
   this->GetHistbyName("QCD"+sub,strVar)->Scale( this->QCD_OSSS(cat) );
+
+  if(reset){
+    int entries = this->GetHistbyName("QCD"+sub,strVar)->GetNbinsX();
+    for(int i = 1; i < entries; i++){
+      if( this->GetHistbyName("QCD"+sub,strVar)->GetBinContent(i) < 0 ) this->GetHistbyName("QCD"+sub,strVar)->SetBinContent(i,0.);
+    }
+  }
 
 }
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
